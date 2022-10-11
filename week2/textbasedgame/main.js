4d674f6417832580e86e22171fc0ef3e3ff3c9e5
Joe <caspar0912@googlemail.com>
2022-10-11 12:42:20 +0100

    add some basic js for interactivity

--- week2/textbasedgame/main.js
+++ week2/textbasedgame/main.js
@@ -4,6 +4,17 @@ const firstNameDisplay = document.querySelector('#name-one');
 const secondNameDisplay = document.querySelector('#name-two');
 const formButton = document.querySelector('#form-submit');
 
+const updateDeviceDisplayName = () => {
+    const deviceDisplay = document.querySelector('#device');
+    const devices = ['fridge', 'toaster', 'scooter', 'phone', 'tablet', 'microwave'];
+    const random = Math.floor(Math.random() * devices.length);
+    deviceDisplay.textContent = devices[random];
+}
+
+window.addEventListener('load', () => {
+    updateDeviceDisplayName();
+})
+
 formButton.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
