a35775696b4dba90b65dee33ef8c241ef7eb32b4
Joe <caspar0912@googlemail.com>
2022-12-05 13:49:28 +0000

    form practice in react

---
+++ week10/form-practice/src/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./modules/Form";
+import Display from "./modules/Display";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Form/>
+      <Display/>
+    </div>
+  );
+}
+
+export default App;
---
+++ week10/form-practice/src/modules/Form.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+const Form = () => {
+  const todaysDate = new Date();
+  const nextMonth = new Date(todaysDate.setMonth(todaysDate.getMonth() + 1));
+  return (
+    <form>
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="first-name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="first-name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            First name
+          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="last-name"
+            id="last-name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="last-name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Last name
+          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group mt-2">
+          <input
+            type="date"
+            name="booking-date"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            min={new Date().toISOString().split('T')[0]}
+            max={nextMonth.toISOString().split('T')[0]}
+            required
+          />
+          <label
+            htmlFor="booking-date"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Booking Date
+          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <label
+            for="countries"
+            class="block mb-2 text-sm font-medium text-gray-500 dark:text-white"
+          >
+            No of people
+          </label>
+          <select
+            id="countries"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="tel"
+            name="telephone"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="telephone"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Telephone Number
+          </label>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
